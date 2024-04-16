410224af96a51dc23c209847e6cd2ab47ce5744e
MarcoColloca <marco.colloca3511@gmail.com>
2024-04-16 11:50:51 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+  export default{
+    data(){
+      return{
+        test: 'Hello!',
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>
+    {{ test }}
+  </h1>
+</template>
+
+<style lang="scss">
+  h1{
+    color: white;
+  }
+</style>
